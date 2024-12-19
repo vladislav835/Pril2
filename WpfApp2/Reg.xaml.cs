05101c7d654f8a1aba82d1839b2edd07db732cf5
zelenskii <zelenskii@308-7>
2024-12-19 15:08:14 +0800

    Добавьте файлы проекта.

---
+++ WpfApp2/Reg.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Reg.xaml
+    /// </summary>
+    public partial class Reg : Page
+    {
+        public Reg()
+        {
+            InitializeComponent();
+            //Btn_Vhod.Visibility = Visibility.Hidden;
+        }
+
+        private void Btn_Vhod_Click(object sender, RoutedEventArgs e)
+        {
+
+            string managerLogin = "manager";
+            string managerPassword = "manager";
+
+            string enteredLogin = textbox1.Text;
+            string enteredPassword = textbox2.Password;
+
+            if (enteredLogin == managerLogin && enteredPassword == managerPassword)
+            {
+                Manager.MainFrame.Navigate(new Vbor());
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+
+        private void Btn_Tour_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new MatPage());
+        }
+    }
+}
---
+++ WpfApp2/Vbor.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Vbor.xaml
+    /// </summary>
+    public partial class Vbor : Page
+    {
+        public Vbor()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new MatPage()); 
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AppPage());
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
